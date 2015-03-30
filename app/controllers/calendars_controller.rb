c78cc68b7209c15ae14d57a8a2ebaaf342942e93
Čeněk Drbal <cendrb@gmail.com>
2015-03-30 23:12:55 +0200

    wfsds

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -11,11 +11,34 @@ class CalendarsController < ApplicationController
       puts day
     end
     puts 'akbar'
-    @templates = @calendar.timetable_template.exercise_templates.where("weekday IN (:weekdays)", weekdays: weekdays).to_a
-    @templates.delete_if do |template|
-      @registered.each do |registered|
-        return (registered.date)..(registered.date + @calendar.therapy.duration_in_minutes.minutes).overlaps?((template.beginning)..(template.beginning + @calendar.therapy.duration_in_minutes.minutes))
-      end
+    @templates = @calendar.timetable_template.exercise_templates#.where("weekday IN (:weekdays)", weekdays: weekdays)
+    
+    @templates.each do |template|
+      puts template
     end
+    
+    # najde
+    if(@registered.minimum("EXTRACT(HOUR FROM date)").nil?)
+      registered_min_hour = 69
+    else
+      registered_min_hour = @registered.minimum("EXTRACT(HOUR FROM date)")
+    end
+    
+    if(@registered.maximum("EXTRACT(HOUR FROM date)").nil?)
+      registered_max_hour = -69
+    else
+      registered_max_hour = @registered.minimum("EXTRACT(HOUR FROM date)")
+    end
+    
+    @first_hour = [registered_min_hour, @templates.minimum(:beginning).hour].min
+    @last_hour = [registered_max_hour, @templates.maximum(:beginning).hour].max + 1
+    
+    # TERRIBLE CODE
+    #@templates = @templates.to_a
+    #@templates.delete_if do |template|
+    #  @registered.each do |registered|
+    #    return (registered.date)..(registered.date + @calendar.therapy.duration_in_minutes.minutes).overlaps?((template.beginning)..(template.beginning + @calendar.therapy.duration_in_minutes.minutes))
+    #  end
+    #end
   end
 end
