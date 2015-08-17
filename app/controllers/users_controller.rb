ce3ee0d20ddf652dc45fef32320baeba89f36ed9
Čeněk Drbal <cendrb@gmail.com>
2015-08-17 11:33:08 +0200

    rwee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_admin, only: [:index, :admin_edit, :admin_update, :show, :administration]
+  before_filter :authenticate_registered, only: [:summary]
   before_filter :self_edit?, only: [:edit, :update, :destroy, :tracked_value_chart]
   before_action :set_user, only: [:show, :edit, :update, :destroy, :admin_edit, :admin_update]
   
