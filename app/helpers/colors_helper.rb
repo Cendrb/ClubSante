26e204f4bbe3252f3db9f5606788576e5a0af941
Cendrb <cendrb@gmail.com>
2015-09-08 22:31:39 +0200

    zidan

---
+++ app/helpers/colors_helper.rb
@@ -0,0 +1,14 @@
+module ColorsHelper
+  def get_calendar_color(object)
+    case object
+      when Exercise
+        return (object.exercise_modification.coach && object.exercise_modification.coach.color_code) ? object.exercise_modification.coach.color_code : Coach.default_color
+      when ExerciseTemplate
+        return (object.coach && object.coach.color_code) ? object.coach.color_code : Coach.default_color
+      when ExerciseModification
+        return (object.coach && object.coach.color_code) ? object.coach.color_code : Coach.default_color
+      else
+        return Coach.default_color
+    end
+  end
+end
