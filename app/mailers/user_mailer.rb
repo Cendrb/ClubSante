82a2cd0c9578c13e156089d84f87af041c3a0088
Čeněk Drbal <cendrb@gmail.com>
2016-04-07 18:22:40 +0200

    [TASK] kana

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
+    @data = {}
     @data[:user] = user
     mail(to: user.email, subject: "Vítejte v rezervačním systému Club Sante")
   end
